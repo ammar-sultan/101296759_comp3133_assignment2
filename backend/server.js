c6e5b6e1ef99757d992bf18142bf87fb77d4356f
ammar <ammar.sultan@georgebrown.ca>
2025-04-05 22:49:00 -0400

    fixed docker files

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,39 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const typeDefs = require("./schemas/schema");
 const resolvers = require("./resolvers/resolver");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "https://comp3133_frontend-domain.com", // Update with the actual frontend domain
+      "http://localhost:4200",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 async function startServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
+
   await server.start();
-  server.applyMiddleware({ app });
-
+  server.applyMiddleware({ app, cors: false });
   const PORT = process.env.PORT || 4000;
+  const HOST = process.env.HOST || "0.0.0.0";
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, HOST, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
     console.log(
-      `GraphQL playground is available at http://localhost:${PORT}/graphql`
+      `🚀 GraphQL playground available at http://${HOST}:${PORT}/graphql`
     );
   });
 }
+
 startServer();
